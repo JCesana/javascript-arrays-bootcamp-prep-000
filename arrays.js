540bb404ac4224a566667efde416849ebe81755b
Josh Cesana <joshuacesana@gmail.com>
2018-05-06 23:25:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
+function addElementToBeginningOfArray(array, element) {
+  var output = [];
+  output = [element, ...array];
+  
+  return output;
+}
+
+function 
